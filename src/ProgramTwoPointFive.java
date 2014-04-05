733b6dce982f6191181ef099e25592b454a16282
skrull1 <Joachim@192.168.0.185>
2014-04-05 10:22:30 -0500

    Adding Decimal Format

---
+++ src/ProgramTwoPointFive.java
@@ -0,0 +1,26 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class ProgramTwoPointFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Create Scanner
+		Scanner input = new Scanner(System.in);
+                
+                // Create Decimal Format object
+                DecimalFormat df = new DecimalFormat("#.##");
+		
+		// Prompt user
+		System.out.print("Enter a subtotal and a gratuity rate: ");
+		
+		double subtotal = input.nextDouble();
+		double gratuity = input.nextDouble() / 100;	
+		
+		System.out.println("The gratuity is " + (int)(subtotal * gratuity  * 100) / 100.0 + " and the total is " + (int)(subtotal * (1 + gratuity) * 100) / 100.0);
+		input.close();
+	}
+
+}
