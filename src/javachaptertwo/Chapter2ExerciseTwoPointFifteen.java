3a999940dc74533828ca9ba632bacc083b2df831
skrull1 <Joachim@192.168.0.185>
2014-04-06 11:04:58 -0500

    Changed to Decimal Format and removed invalid package

--- src/javachaptertwo/Chapter2ExerciseTwoPointFifteen.java
+++ src/javachaptertwo/Chapter2ExerciseTwoPointFifteen.java
@@ -1,10 +1,13 @@
+
+import java.text.DecimalFormat;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package test;
+
 
 /**
  *
@@ -18,9 +21,12 @@ public class Chapter2ExerciseTwoPointFifteen {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        // Create DecimalFormat Object
+        DecimalFormat df = new DecimalFormat("$###,###.##");
+        
         double accountValue = 100;
         accountValue = 100 * (1 + 0.00417);
-        System.out.println("First Month: " + (int)(accountValue * 1000) / 1000.0);
+        /* System.out.println("First Month: " + (int)(accountValue * 1000) / 1000.0);
         accountValue = (100 + accountValue) * (1 + 0.00417);
         System.out.println("Second Month: " +(int)(accountValue * 1000) / 1000.0);
         accountValue = (100 + accountValue) * (1 + 0.00417);
@@ -30,7 +36,19 @@ public class Chapter2ExerciseTwoPointFifteen {
         accountValue = (100 + accountValue) * (1 + 0.00417);
         System.out.println("Fifth Month: " +(int)(accountValue * 1000) / 1000.0);
         accountValue = (100 + accountValue) * (1 + 0.00417);
-        System.out.println("Sixth Month: " + (int)(accountValue * 1000) / 1000.0);
+        System.out.println("Sixth Month: " + (int)(accountValue * 1000) / 1000.0); */
+        
+        System.out.println("First Month: " + df.format(accountValue));
+        accountValue = (100 + accountValue) * (1 + 0.00417);
+        System.out.println("Second Month: " + df.format(accountValue));
+        accountValue = (100 + accountValue) * (1 + 0.00417);
+        System.out.println("Third Month: " + df.format(accountValue));
+        accountValue = (100 + accountValue) * (1 + 0.00417);
+        System.out.println("Fourth Month: " + df.format(accountValue));
+        accountValue = (100 + accountValue) * (1 + 0.00417);
+        System.out.println("Fifth Month: " + df.format(accountValue));
+        accountValue = (100 + accountValue) * (1 + 0.00417);
+        System.out.println("Sixth Month: " + df.format(accountValue));
     }
     
 }
