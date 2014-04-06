88ff31259d807705bcd8f8dbca88aca740c988fa
skrull1 <Joachim@192.168.0.185>
2014-04-06 10:30:33 -0500

    Changed to Decimal Format and removed invalid package

--- src/javachaptertwo/Chapter2ExerciseTwoPointFourteen.java
+++ src/javachaptertwo/Chapter2ExerciseTwoPointFourteen.java
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 
-package test;
 
+
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -19,15 +20,21 @@ public class Chapter2ExerciseTwoPointFourteen {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Create Scanner Object
         Scanner input = new Scanner(System.in);
+        
+        // Create DecimalFormat Object
+        DecimalFormat df = new DecimalFormat("#.####");
+        
         System.out.print("Enter weight in pounds: ");
         double poundsEntered = input.nextDouble();
         System.out.print("Enter height in inches: ");
         double heightEntered = input.nextDouble();
         
-        double bMI = (int)((poundsEntered * 0.45359237) / Math.pow(heightEntered * 0.0254, 2) * 10000) / 10000.0;
+        double bMI = (poundsEntered * 0.45359237) / Math.pow(heightEntered * 0.0254, 2);
         
-        System.out.println("BMI is "+ bMI);
+        System.out.println("BMI is "+ df.format(bMI));
         
                 
     }
