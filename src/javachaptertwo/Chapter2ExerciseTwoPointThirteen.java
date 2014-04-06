f2dc74c3bcab50885f5326358aa93810094d28b5
skrull1 <Joachim@192.168.0.185>
2014-04-06 10:26:45 -0500

    Changed to Decimal Format and removed invalid package

--- src/javachaptertwo/Chapter2ExerciseTwoPointThirteen.java
+++ src/javachaptertwo/Chapter2ExerciseTwoPointThirteen.java
@@ -6,6 +6,7 @@
 
 package test;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -19,7 +20,14 @@ public class Chapter2ExerciseTwoPointThirteen {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Create Scanner Object
         Scanner input = new Scanner(System.in);
+        
+        // Create Decimal Format Object
+        DecimalFormat df = new DecimalFormat("#.##");
+        
+        
         System.out.print("Enter investment amount: ");
         double iAmount = input.nextDouble();
         System.out.print("Enter monthly interst rate: ");
@@ -31,7 +39,7 @@ public class Chapter2ExerciseTwoPointThirteen {
         double cYears = nYears * 12;
         
         double futureInvestmentValue = iAmount * Math.pow(cInterest, cYears);
-        System.out.println("Accumulated value is " + (int)(futureInvestmentValue * 100) / 100.0);
+        System.out.println("Accumulated value is " + df.format(futureInvestmentValue));
         
 
     }
